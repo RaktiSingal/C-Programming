acd19e8eb3e8678ae87527f8a9dd77cb7ad1a064
RaktiSingal <raktisingal83@gmail.com>
2020-11-22 19:52:27 +0530

    Program to calculate area of rectangle

--- 6-program_for_area_of_rectangle.c
+++ 6-program_for_area_of_rectangle.c
@@ -6,7 +6,7 @@ printf("Enter length for rectangle");
 scanf("%f",&length);                      // taking value of length ,using & is neccesary while taking value
 printf("Enter breath for rectangle");
 scanf("%f",&breath);
-area= length * breath;                   // multiplying values of length and breath to get area
+area= length * breath;                   // multiplying values of length and breath to get area of rectangle
 printf("Area of rectangle is %f",area);    // printing area calculated from values taken from user , this does not require any & as if we will do & it will print address of area (where it is stored)
 return 0;
 }
