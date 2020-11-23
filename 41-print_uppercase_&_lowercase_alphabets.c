c5b7048da7fe64009e25df0fb1e141c51d215be7
RaktiSingal <raktisingal83@gmail.com>
2020-11-23 22:03:47 +0530

    Printing Uppercase and Lowercase alphabets

---
+++ 41-print_uppercase_&_lowercase_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main
+{
+int i,n;
+printf("Upper case alphabets");
+for(i=65;i<=90;i++)  
+{
+printf("\n %c",i);   
+}
+printf("Lower case Alphabets");
+for(i=97;i<=122;i++)  
+{
+printf("\n %c",i);   
+}
+return 0;
+}
