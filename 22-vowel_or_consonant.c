a099b29825df9213cc55e4531ed7f8fdf8302260
RaktiSingal <raktisingal83@gmail.com>
2020-11-23 13:10:28 +0530

    Program to check alphabet is Vowel/Consonant

---
+++ 22-vowel_or_consonant.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char alph;
+printf("Enter alphabet to check it is vowel or consonant:");
+scanf("%c",&alph);
+if(alph=='a' || alph=='e' || alph=='i' || alpha=='o' || alpha=='u')
+   printf("\n Vowel");
+else 
+  printf("\n Consonant");
+  return 0;
+}
