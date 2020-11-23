32e04937e8faa9735510ce28062da25b5169e11f
RaktiSingal <raktisingal83@gmail.com>
2020-11-23 11:35:55 +0530

    Program to find maximum out of 2 numbers

---
+++ 17-maximum_out_of_2_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2;
+printf("Enter numbers to compare:");
+scanf("%d %d",&num1,&num2);
+if(num1>num2)
+printf("\n %d is greater",num1);
+else
+printf("\n %d is greater",num2);
+return 0;
+}
