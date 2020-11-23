00049eecdccd751b68bb4ceb432bb07012bfcb7c
RaktiSingal <raktisingal83@gmail.com>
2020-11-23 11:45:16 +0530

    Program to calculate maximum out of 3 numbers

--- 18-maximum_of_3_numbers.c
+++ 18-maximum_of_3_numbers.c
@@ -7,14 +7,14 @@ scanf("%d %d %d",&num1,&num2,&num3);
    if(num1>num2)
    {
     if(num1>num3)
-    printf("%d is greater",num1);
+    printf("\n %d is greater",num1);
    }
    else if(num2>num1)
    {
      if(num2>num3)
-     printf("%d is greater",num2);
+     printf("\n %d is greater",num2);
     } 
   else
-  printf("%d is greater",num3);
+  printf("\n %d is greater",num3);
 return 0;
 }
